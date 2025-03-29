0530693b47b5c31a427d2bf5d1570a0d5963b381
karim <kareem@inviacap.com>
2025-03-29 07:51:55 +0200

    bruh

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,6 @@ function App() {
 
   const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-  console.log(PUBLISHABLE_KEY)
   if (!PUBLISHABLE_KEY) {
     throw new Error("Missing Publishable Key")
   }
