2ee75a58e82b964df27bf407641753ab1cd736b6
karim <kareem@inviacap.com>
2025-04-06 00:29:01 +0200

    o

--- src/pages/LandingPage/Hero.jsx
+++ src/pages/LandingPage/Hero.jsx
@@ -8,10 +8,28 @@ export default function HeroSection() {
             <div className="hero-text-container">
                 <h1>
                     <p>Quieter Clinic</p>
-                    <p>Happier Dentists</p>
+                    <p>Happier Dentist</p>
                 </h1>
                 <p>Reduce compressor noise to below 40 dB for a calm, patient-friendly environment. Silence that enhances care and comfort.</p>
                 <div className='hero-button-container'>
+                    <div className='icons-container-mobile'>
+                        <a
+                            href="https://www.instagram.com/dental.dynamics1?igsh=NzZkbXZoaDdqem9x"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className='icon'
+                        >
+                            <FaInstagram size={24} />
+                        </a>
+                        <a
+                            href="https://www.facebook.com/share/1GGDfV6D52/?mibextid=wwXIfr"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className='icon'
+                        >
+                            <FaFacebook size={24} />
+                        </a>
+                    </div>
                     <Link to={'/shop'} className='hero-button-container-Link' style={{ textDecoration: 'none', color: 'white' }}>
                         <button className='hero-button-container-button'>
                             Order Yours Now
@@ -24,18 +42,18 @@ export default function HeroSection() {
                     <img src={productimage} width={450} />
                 </div>
                 <div className='icons-container'>
-                    <a 
-                        href="https://www.instagram.com/dental.dynamics1?igsh=NzZkbXZoaDdqem9x" 
-                        target="_blank" 
-                        rel="noopener noreferrer" 
+                    <a
+                        href="https://www.instagram.com/dental.dynamics1?igsh=NzZkbXZoaDdqem9x"
+                        target="_blank"
+                        rel="noopener noreferrer"
                         className='icon'
                     >
                         <FaInstagram size={24} />
                     </a>
-                    <a 
-                        href="https://www.facebook.com/share/1GGDfV6D52/?mibextid=wwXIfr" 
-                        target="_blank" 
-                        rel="noopener noreferrer" 
+                    <a
+                        href="https://www.facebook.com/share/1GGDfV6D52/?mibextid=wwXIfr"
+                        target="_blank"
+                        rel="noopener noreferrer"
                         className='icon'
                     >
                         <FaFacebook size={24} />
